0e61e7871d97e60f9f71621b3040f2836e0d9aa0
Ednelson Alencar Pupo <ednelson.alencar@gmail.com>
2021-06-08 22:31:51 -0300

    Implementando o Health Check com Spring Boot Actuator

--- src/main/java/br/com/zup/academy/ednelson/proposta/novaproposta/ConsultaRestricaoClient.java
+++ src/main/java/br/com/zup/academy/ednelson/proposta/novaproposta/ConsultaRestricaoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "http://localhost:9999/api", name = "consulta")
+@FeignClient(url = "${zup.analise.financeira}", name = "consultaRestricao")
 public interface ConsultaRestricaoClient {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/solicitacao")
