da53734d06903fab885f32a2a2f598484386056c
Ednelson Alencar Pupo <ednelson.alencar@gmail.com>
2021-07-07 10:37:29 -0300

    Corrigindo keycloak para rodar app dentro e fora do docker

---
+++ src/main/java/br/com/zup/academy/ednelson/proposta/security/DevSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.zup.academy.ednelson.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Profile("dev")
+@Configuration
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
+		
+	}
+}
--- src/main/java/br/com/zup/academy/ednelson/proposta/security/SecurityConfiguration.java
+++ src/main/java/br/com/zup/academy/ednelson/proposta/security/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package br.com.zup.academy.ednelson.proposta.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
+@Profile("prod")
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
